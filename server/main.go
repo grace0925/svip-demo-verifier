946c3fb8d61c4ba5c96ae1055c29485d15224e44
GraceLiu <graceliu0925@gmail.com>
2020-01-16 14:54:33 -0500

    change favicon

--- server/main.go
+++ server/main.go
@@ -10,8 +10,8 @@ import (
 func main() {
 	r := mux.NewRouter()
 
-	r.Handle("/", http.FileServer(http.Dir("./template/")))
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	//r.Handle("/", http.FileServer(http.Dir("./template/")))
+	//r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
