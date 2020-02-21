6bd292ef3a88242ecda78e458f1d5aadfff1dab9
GraceLiu <graceliu0925@gmail.com>
2020-02-21 16:39:04 -0500

    Modify header

--- issuer/client/src/components/header.js
+++ issuer/client/src/components/header.js
@@ -4,22 +4,32 @@ import '../stylesheets/common.css'
 import Flag from "../assets/flag.png"
 import USCIS from '../assets/USCIS.png'
 
-import {Navbar, Nav, Container} from 'react-bootstrap'
+import {Navbar, Nav, Container, Button} from 'react-bootstrap'
+
 
 function Header() {
+    function signup() {
+        console.log("signing up")
+    }
     return (
         <div>
             <Container className="small-font mt-2">
                 <img src={Flag} alt="Mini american flag" width="20px" height="14px" className="mr-1 mb-1"/>
                     Department of Homeland Security
             </Container>
-            <Navbar variant="dark" className="darkblue">
+            <Navbar collapseOnSelect expand="lg" variant="dark" className="darkblue">
                 <Container>
                     <Navbar.Brand fixed="top" href="#home" className="times-new-roman-font">
                         <img src={USCIS} alt="logo" width="56px" height="56px" className="ml-1 mr-2"/>
                         SVIP Issuer
                     </Navbar.Brand>
-                    <Nav className="mr-auto"> </Nav>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                    <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+                        <Nav>
+                            <Nav.Link className="nav-login">Log in</Nav.Link>
+                            <Nav.Link className="nav-signup" href="/infoForm">Sign up</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </div>
