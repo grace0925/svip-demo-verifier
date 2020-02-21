0f8d88c4c79afde7dd5ea8331a6544c563486ac0
GraceLiu <graceliu0925@gmail.com>
2020-02-21 13:37:23 -0500

    Update header style

--- issuer/client/src/components/header.js
+++ issuer/client/src/components/header.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import '../stylesheets/common.css'
-import {Navbar, Nav} from 'react-bootstrap'
+import Flag from "../assets/flag.png"
+import USCIS from '../assets/USCIS.png'
+import {Navbar, Nav, Container} from 'react-bootstrap'
 
 function Header() {
     return (
         <div>
-            <Navbar fixed="top" variant="dark" className="darkblue">
-                <Navbar.Brand href="#home">SVIP Issuer</Navbar.Brand>
-                <Nav className="mr-auto"> </Nav>
+            <Container className="small-font mt-2">
+                <img src={Flag} alt="Mini american flag" width="20px" height="14px" className="mr-1 mb-1"/>
+                    Department of Homeland Security
+            </Container>
+            <Navbar variant="dark" className="darkblue">
+                <Container>
+                    <Navbar.Brand fixed="top" href="#home" className="times-new-roman-font">
+                        <img src={USCIS} alt="logo" width="58px" height="58px" className="ml-1 mr-2"/>
+                        SVIP Issuer
+                    </Navbar.Brand>
+                    <Nav className="mr-auto"> </Nav>
+                </Container>
             </Navbar>
         </div>
     );
