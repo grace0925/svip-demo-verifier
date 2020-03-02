4edb584b58b41bb16d20080c13fb1d5993007a9d
GraceLiu <graceliu0925@gmail.com>
2020-03-02 11:58:47 -0500

    Confirm password and signup form validation

--- wallet/client/src/components/signup.js
+++ wallet/client/src/components/signup.js
@@ -14,6 +14,7 @@ class Signup extends React.Component{
             showModal: this.props.showModal,
             username: "",
             password: "",
+            confirm: "",
         }
         this.signup = this.signup.bind(this);
         this.cancel = this.cancel.bind(this);
@@ -44,26 +45,46 @@ class Signup extends React.Component{
         })
     };
 
+    submitHandler = e => {
+        e.preventDefault()
+        if (this.state.password !== this.state.confirm) {
+
+        } else {
+            this.signup()
+        }
+    }
+
     render() {
-        const {username, password} = this.state;
+        const {username, password, confirm} = this.state;
         return(
             <Modal className="square-modal" show={this.state.showModal} onHide={this.cancel}>
                 <Modal.Header className="signup-header">Sign Up</Modal.Header>
                 <Modal.Body>
                     <p className="ml-2 montserrat-fonts">Your username and password will be used to log into your wallet. Your account is only for you. Do not create a shared account with your family or friends. </p>
-                    <Form className="px-2 mt-4">
+                    <Form onSubmit={this.submitHandler} className="px-2 mt-4">
                         <Form.Group>
                             <Form.Label>Username</Form.Label>
                             <Form.Control type="username" name="username" value={username} onChange={this.formChangeHandler}/>
                         </Form.Group>
-                        <Form.Group>
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" name="password" value={password} onChange={this.formChangeHandler}/>
-                        </Form.Group>
+                        <div className={`${(this.state.password !== this.state.confirm) && (this.state.confirm !== '') ? 'error-state' : ''}`}>
+                            <Form.Group>
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" name="password" value={password} onChange={this.formChangeHandler}/>
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label>Confirm Password</Form.Label>
+                                {(this.state.password !== this.state.confirm) && (this.state.confirm !== '') ?
+                                    (<Form.Text className="error-text">The passwords don't match.</Form.Text>) : null}
+                                <Form.Control type="password" name="confirm" value={confirm} onChange={this.formChangeHandler}/>
+                            </Form.Group>
+                        </div>
                     </Form>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button onClick={this.signup}>Sign up</Button>
+                    {((this.state.password !== this.state.confirm) && (this.state.confirm !== '')) ||
+                    (this.state.username === '' || this.state.password === '' || this.state.confirm === '') ?
+                        <Button type="submit" disabled>Sign up</Button> :
+                        <Button type="submit">Sign up</Button>}
                     <Button onClick={this.cancel}>Cancel</Button>
                 </Modal.Footer>
             </Modal>
